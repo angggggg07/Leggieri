bf997a4ea6167ff9ac358531dd9386c96f2a24c8
Angelo Leggieri <leggieriangelo589@gmail.com>
2025-05-02 12:49:34 +0200

    aggiunta rimozione libro e visualizzazione della libreria

--- source/index.js
+++ source/index.js
@@ -3,6 +3,7 @@ const prompt = promptSync();
 import * as f from './utils.js';
 
 let scelta;
+let libreria=[];
 
 do{
 
@@ -12,15 +13,22 @@ console.log("\nMenu\n 1-Aggiungi Libro.\n 2-Rimuovi.\n 3-Visualizza libreria. \n
 switch(scelta)
 {
     case "1":
-        case "1":
-            const titolo = prompt("Titolo: ");
-            const autore = prompt("Autore: ");
-            const genere = prompt("Genere: ");
-            const isbn = prompt("ISBN: ");
-            f.aggiungiLibro(titolo, autore, genere, isbn);
+        const titolo = prompt("Titolo: ");
+        const autore = prompt("Autore: ");
+        const genere = prompt("Genere: ");
+        const isbn = prompt("ISBN: ");
+        libreria.push(f.aggiungiLibro(titolo,autore,genere,isbn));
         break;
 
-        
+    case "2":
+        const isbnRimuovere=prompt("Inserisci l'isbn del libro da rimuovere: ");
+        f.rimuoviLibro(libreria,isbnRimuovere);
+        break;
+    
+    case "3":
+        f.visualizzaLibreria(libreria);
+        break;
+
     default:
         console.log("Scelta non valida!");
 
--- source/utils.js
+++ source/utils.js
@@ -1,8 +1,23 @@
-let libreria=[];
-
 export function aggiungiLibro(titolo,autore,genere,isbn)
 {
     const nuovoLibro = {titolo, autore, genere, isbn};
-    libreria.push(nuovoLibro);
     console.log("Libro aggiunto con successo!");
+    return nuovoLibro
+}
+
+export function rimuoviLibro(libreria, isbn)
+{
+    let isbnlib = libreria.find(libro => libro.isbn == isbn);
+    let i = libreria.findIndex(libro => libro.isbn == isbn);
+    if(isbnlib.isbn == isbn){
+        libreria.splice(i,1);
+        console.log("Libro rimosso con successo!");
+    }
+    else
+        console.log("Libro non trovato");
+    console.log(libreria);
+}
+
+export const visualizzaLibreria =(libreria)=>{
+    console.log(libreria);
 }
