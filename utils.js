bf94fafdfc4b341fa015ef9653e5a2876b15bcef
Angelo Leggieri <leggieriangelo589@gmail.com>
2025-05-06 08:48:26 +0200

    Eliminazione file  aggiuntivi

--- utils.js
+++
@@ -1,8 +0,0 @@
-let libreria=[];
-
-export function aggiungiLibro(titolo,autore,genere,isbn)
-{
-    const nuovoLibro = {titolo, autore, genere, isbn};
-    libreria.push(nuovoLibro);
-    console.log("Libro aggiunto con successo!");
-}
